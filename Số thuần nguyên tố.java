4c02728d3e1adeac9f2466dec05f911111def59f
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-21 11:04:37 +0700

    Create Số thuần nguyên tố.java

---
+++ Số thuần nguyên tố.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static boolean primeCheck(int n){
+        if(n < 2) return false;
+        for(int i = 2;i <= Math.sqrt(n);i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+    public static boolean csnt(int n){
+        while(n != 0){
+            if(n % 10 != 2 && n % 10 != 5 && n % 10 != 7 && n % 10 != 3){
+                return false;
+            }
+            n /= 10;
+        }
+        return true;
+    }
+    public static boolean check(int n){
+        int sum = 0;
+        while(n != 0){
+            sum += n % 10;
+            n/=10;
+        }
+        return primeCheck((sum));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int cnt = 0;
+        for(int i = a;i <= b;i++){
+            if(csnt(i) && check(i) && primeCheck(i)){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
