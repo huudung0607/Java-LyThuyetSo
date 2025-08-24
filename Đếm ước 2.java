0e8af1e413ad1b3743e5f2dd017357c167cd4c74
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-24 10:43:49 +0700

    Create Đếm ước 2.java

---
+++ Đếm ước 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long sum = 0;
+        long n = sc.nextLong();
+        for(int i = 1; i <= Math.sqrt(n);i++){
+            if(n % i == 0){
+                sum ++;
+                if(i != n/i){
+                    sum ++;
+                }
+            }
+        }
+        System.out.print(sum);
+    }
+}
