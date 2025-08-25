a041d8f9af425f4b51a4bcd6d6b86924517afc09
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-25 17:13:22 +0700

    Create Fibonacci 1.java

---
+++ Fibonacci 1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    public static boolean check(long n){
+        if(n == 0 || n == 1) return true;
+        long []F = new long[100];
+        F[0] = 0; F[1] = 1;
+        for(int i = 2;i <= 92;i++){
+            F[i] = F[i - 1] + F[i - 2];
+        }
+        for(int i = 2; i <= 92;i++){
+            if(n == F[i]) return true;
+        }
+        return false;
+    }
+    private static int mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long []F = new long[100];
+        F[0] = 0; F[1] = 1;
+        for(int i = 2;i <= 92;i++){
+            F[i] = F[i - 1] + F[i - 2];
+        }
+        for(int i = 1; i <= n;i++){
+            System.out.println(F[i] + " ");
+        }
+    }
+}
